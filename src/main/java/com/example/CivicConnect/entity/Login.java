4824444b2e0794527c4e259a89e83899d18e2c5a
Saifa2102 <saifakhatun77@gmail.com>
2025-01-19 16:23:32 +0530

    2nd commit, login and register

--- src/main/java/com/example/CivicConnect/entity/Login.java
+++ src/main/java/com/example/CivicConnect/entity/Login.java
@@ -1,19 +1,17 @@
 package com.example.CivicConnect.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Setter
 @Getter
+@Table(name = "Login_Details")
 public class Login {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
     private String email;
     private String password;
 }
